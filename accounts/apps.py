5edf589cef85a9005bc8a1087cd8bbed083a003f
Qobiljon <qobiljonsharipov33@gmail.com>
2025-11-17 13:39:16 +0500

    Edited for deploy

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,25 @@
 from django.apps import AppConfig
-
+from django.db.utils import OperationalError
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        from django.db.models.signals import post_migrate
+
+        def create_default_admin(sender, **kwargs):
+            User = get_user_model()
+            try:
+                if not User.objects.filter(username="admin").exists():
+                    User.objects.create_superuser(
+                        username="admin",
+                        password="admin123",
+                        full_name="Default Admin"
+                    )
+                    print("✅ Default superuser yaratildi: admin / admin123")
+            except OperationalError:
+                pass
+
+        post_migrate.connect(create_default_admin, sender=self)
---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+set -e  
+
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+
+exec gunicorn config.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --workers 4
